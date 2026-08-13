9e94d08cdcc39b007b3fcdc0540f5d2b38a8fe82
pryins-bit <pr.yins@gmail.com>
2026-08-13 15:18:11 +0900

    MegaSwing: use native Gutenberg Block type

--- routes/dashboard/megaswing-blocks-compat.d.ts
+++
@@ -1,7 +0,0 @@
-import '@wordpress/blocks';
-
-declare module '@wordpress/blocks' {
-	export type BlockInstance<
-		Attributes extends Record< string, unknown > = Record< string, unknown >,
-	> = Block< Attributes >;
-}
